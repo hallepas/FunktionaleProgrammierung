d2067b1cf516446913d6a80f508a5d6fd14ddabc
Pascal Haller <pascal_haller@hotmail.com>
2016-02-25 22:30:09 +0100

    add fsharp project

---
+++ firsttry/AssemblyInfo.fs
@@ -0,0 +1,41 @@
+﻿namespace firsttry.AssemblyInfo
+
+open System.Reflection
+open System.Runtime.CompilerServices
+open System.Runtime.InteropServices
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[<assembly: AssemblyTitle("firsttry")>]
+[<assembly: AssemblyDescription("")>]
+[<assembly: AssemblyConfiguration("")>]
+[<assembly: AssemblyCompany("")>]
+[<assembly: AssemblyProduct("firsttry")>]
+[<assembly: AssemblyCopyright("Copyright ©  2016")>]
+[<assembly: AssemblyTrademark("")>]
+[<assembly: AssemblyCulture("")>]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[<assembly: ComVisible(false)>]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[<assembly: Guid("cb84da58-8314-4504-a274-d2793512f1f1")>]
+
+// Version information for an assembly consists of the following four values:
+// 
+//       Major Version
+//       Minor Version 
+//       Build Number
+//       Revision
+// 
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [<assembly: AssemblyVersion("1.0.*")>]
+[<assembly: AssemblyVersion("1.0.0.0")>]
+[<assembly: AssemblyFileVersion("1.0.0.0")>]
+
+do
+    ()
---
+++ firsttry/Uebung1.fsx
@@ -0,0 +1,53 @@
+﻿#r @"exerciseTester.dll"
+
+//#r @"C:\Users\Pascal\Documents\Visual Studio 2015\Projects\firsttry\firsttry\exerciseTester.dll"
+//Aufgabe 2a
+let rec exp3 n=
+    if n<1 then 1
+    else 3*exp3 (n-1)
+
+let rec exp3_2 = pown 3 //angewendet auf basis 3
+
+Test.test2a exp3
+
+//Aufgabe 2b
+let rec fak n=
+    if n = 0 
+    then 1 
+    else n * fak (n - 1)
+
+let rec fak2 x = List.fold (*) 1 [1..x]
+
+
+Test.test2b fak
+
+//Aufgabe 2d
+let rec sum f ug og =
+   if ug > og then 0
+   else (f ug) + sum f (ug+1) og
+
+let sum' f ug og = List.fold (fun x y -> x + f y) 0 [ug..og]
+let sum'' (f: int -> int) ug og = List.sum(List.map f [ug..og])
+
+let sum''' (f: int -> int) ug og = 
+    [ug..og]  |> List.map f  //zuerst wird dieses hinten angefügt
+              |> List.sum   //danach wird auf diesem das vorhergehende angewendet
+//let (|>) f g = g f
+3 |> fun x -> x*x
+  |> fun x -> x+x
+  |> (+) <| 8
+//Test.test2d sum
+
+//Aufgabe 2c
+let rec SumSq m n=
+   sum (fun x->x*x) m n
+
+SumSq 5 2
+Test.test2c SumSq
+
+
+
+//Aufgabe 4
+
+
+
